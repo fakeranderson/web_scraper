818c3bbe6c45bd1ad48d0b9e9e5805850ce626af
Ryan Anderson <ryan_mba@MacBook-Pro.local>
2020-07-29 01:04:00 -0400

    extracted something promising into a collection

--- src/web_scraper/core.clj
+++ src/web_scraper/core.clj
@@ -7,4 +7,8 @@
   (def sony-store-url "https://store.playstation.com/en-us/grid/STORE-MSF77008-ALLGAMES/1?direction=desc&gameType=ps4_full_games&releaseDate=last_7_days&smcid=pdc%3Aen-us%3Aexplore-games-ps4-games%3Aprimary%2520nav%3Amsg-shop%3Adigital-games-and-services&sort=release_date")
   ; First grab the link from each title page
   (def website-sony-store (html/html-resource (java.net.URL. sony-store-url)))
-  (print (html/select website-sony-store [:div.grid-cell__body])))
+  ; (print (html/select website-sony-store [:div.grid-cell__title :span])))
+  (def selections (html/select website-sony-store [:a.internal-app-link]))
+   (println (partition 3 selections))
+
+  )
